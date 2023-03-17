5bcea45f72e50b24c1f32288d8213f7f87153f44
flat1ander <thematthewlane@gmail.com>
2023-03-16 23:34:23 -0400

    Take one down and pass it around completed.

--- server.js
+++ server.js
@@ -13,33 +13,30 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 app.get('/magic/:phrase', (req, res) => {
     const magicAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
     let randomPhrase = Math.floor(Math.random() * 20)
-    res.send(`
-    <html>
-        <body>
-            <h1>${req.params.phrase}? The answer is... ${magicAnswers[randomPhrase]}</h1>
-        </body>
-    </html>`)
+    res.send(`<h1>${req.params.phrase}? The answer is... ${magicAnswers[randomPhrase]}</h1>`)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, () => {
     console.log('Server started and listening on port 3000')
 })
+
+app.get('/', (req, res) => {
+    res.send(`
+        99 bottles of beer on the wall
+        <a href ="http://localhost:3000/98">Take one down...</a>`
+    )
+})
+
+app.get('/0', (req, res) => {
+    res.send(`0 Bottles of Beer on the wall </br> <a href = "http://localhost:3000">Line 'em back up</a>`)
+})
+
+let reducedNumber = 97;
+
+app.get('/:number', (req, res) => {
+    res.send(`${req.params.number} bottles of beer on the wall...
+    <a href ="http://localhost:3000/${reducedNumber}">Take one down...</a>
+    `);
+    reducedNumber--;
+})
+
