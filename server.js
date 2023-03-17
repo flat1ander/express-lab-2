4bd8633984fc50619d48c5276773e80ae49e2a0f
flat1ander <thematthewlane@gmail.com>
2023-03-16 23:04:46 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`${totalTip}`);
 })
 
-
-
-
+app.get('/magic/:phrase', (req, res) => {
+    const magicAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let randomPhrase = Math.floor(Math.random() * 20)
+    res.send(`
+    <html>
+        <body>
+            <h1>${req.params.phrase}? The answer is... ${magicAnswers[randomPhrase]}</h1>
+        </body>
+    </html>`)
+})
 
 
 
